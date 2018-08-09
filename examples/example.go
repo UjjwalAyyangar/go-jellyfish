734d9931efb35fedab3979fef829283b83e0e10a
Ujjwal Ayyangar <ujjwal.097@gmail.com>
2018-08-09 22:38:53 +0530

    Some tests added

--- examples/example.go
+++ examples/example.go
@@ -10,7 +10,7 @@ func main(){
     fmt.Println(jellyfish.Jaro_distance("Hello","World"))
     fmt.Println(jellyfish.Jaro_winkler("ABC","ABD"))
     fmt.Println(jellyfish.Hamming_distance("ABC","ABD"))
-    fmt.Println(jellyfish.Match_rating_comparison("ABC","LOL"))
+    fmt.Println(jellyfish.Match_rating_comparison("hello","yellow"))
     fmt.Println(jellyfish.Match_rating_codex("Jellyfish"))
     fmt.Println(jellyfish.Soundex("Jellyfish"))
     fmt.Println(jellyfish.Nysiis("JellyFish"))
--- tests/test.go
+++ tests/test.go
@@ -46,8 +46,6 @@ func TestLevenshtein_distance(){
             fmt.Println(inp1,inp2,output,calc_output)
         }
 
-
-
     }
 
 }
@@ -60,8 +58,6 @@ func TestJaro_distance(){
 
         inp1,inp2 := data[i][0],data[i][1]
         output, _ := strconv.ParseFloat(data[i][2],64)
-        fmt.Println(inp1,inp2,output)
-        /*
         calc_output := jellyfish.Jaro_distance(inp1,inp2)
 
         if calc_output == output{
@@ -72,16 +68,99 @@ func TestJaro_distance(){
             fmt.Println(inp1,inp2,output,calc_output)
         }
 
-        */
+    }
 
+}
 
+func TestJaro_winkler(){
+    data := ReadFile("./test_data/jaro_winkler.csv")
+    l := len(data)
+    for i:=0; i< l; i++ {
+
+        inp1,inp2 := data[i][0],data[i][1]
+        output, _ := strconv.ParseFloat(data[i][2],64)
+        calc_output := jellyfish.Jaro_winkler(inp1,inp2)
+
+        if calc_output == output{
+            fmt.Println("TRUE")
+            fmt.Println(calc_output,output)
+        }else {
+            fmt.Println("False")
+            fmt.Println(inp1,inp2,output,calc_output)
+        }
+    }
+
+}
+
+func TestHamming_distance(){
+    data := ReadFile("./test_data/hamming_distance.csv")
+    l := len(data)
+    for i:=0; i< l; i++ {
+
+        inp1,inp2 := data[i][0],data[i][1]
+        output, _ := strconv.ParseFloat(data[i][2],64)
+        calc_output := jellyfish.Hamming_distance(inp1,inp2)
+
+        if calc_output == int(output){
+            fmt.Println("TRUE")
+            fmt.Println(calc_output,output)
+        }else {
+            fmt.Println("False")
+            fmt.Println(inp1,inp2,output,calc_output)
+        }
+    }
+
+}
+
+func TestMatch_rating_comparison(){
+    data := ReadFile("./test_data/match_rating.csv")
+    l := len(data)
+    for i:=0; i< l; i++ {
+
+        inp1,inp2 := data[i][0],data[i][1]
+        output, _ := strconv.ParseBool(data[i][2])
+        _,calc_output := jellyfish.Match_rating_comparison(inp1,inp2)
+
+        if calc_output == output{
+            fmt.Println("TRUE")
+            fmt.Println(calc_output,output)
+        }else {
+            fmt.Println("FALSE")
+            fmt.Println(inp1,inp2,output,calc_output)
+        }
+    }
+
+}
+
+func TestDamerau_levenshtein_distance(){
+    data := ReadFile("./test_data/damerau_levenshtein_distance.csv")
+    l := len(data)
+    for i:=0; i< l; i++ {
+
+        inp1,inp2 := data[i][0],data[i][1]
+        output, _ := strconv.Atoi(data[i][2])
+        calc_output := jellyfish.Damerau_levenshtein_distance(inp1,inp2)
+        if calc_output == output{
+            fmt.Println("TRUE")
+            fmt.Println(calc_output,output)
+        }else {
+            fmt.Println("FALSE")
+            fmt.Println(inp1,inp2,output,calc_output)
+        }
     }
 
 }
 
 
+
+
 func main(){
     //ReadFile("./test_data/levenshtein_distance.csv")
     //TestLevenshtein_distance()
-    TestJaro_distance()
+    TestDamerau_levenshtein_distance()
+    //TestJaro_distance()
+    //TestJaro_winkler()
+    //TestHamming_distance()
+    //TestMatch_rating_comparison()
+
 }
